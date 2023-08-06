902150061b1e2699adffe075b1050d44206ba58e
Muhammad Saqib <mohdsaqib.rx@gmail.com>
2023-08-06 20:54:08 +0530

    Added Home Page Widgets
    Added Stories
    Added Chats
    Added New Stories Button

--- lib/views/home/home_widgets.dart
+++ lib/views/home/home_widgets.dart
@@ -1,39 +1,80 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import '../../constants/colors.dart';
 
 Widget addStoryWidget({
   required double size,
   required IconData iconData,
+  required String text,
 }) {
-  return Container(
-    width: size,
-    height: size,
-    decoration: BoxDecoration(
-      shape: BoxShape.circle,
-      gradient: LinearGradient(
-        colors: [
-          greenGradient.lightShade,
-          greenGradient.darkShade,
-        ],
+  return Column(
+    children: [
+      Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          gradient: LinearGradient(
+            colors: [
+              greenGradient.lightShade,
+              greenGradient.darkShade,
+            ],
+          ),
+        ),
+        child: Icon(
+          iconData,
+          color: Colors.white,
+        ),
       ),
-    ),
-    child: Icon(iconData, color: Colors.white),
+      const SizedBox(height: 10),
+      Text(
+        text,
+        style: TextStyle(color: grayColor.lightShade),
+      ),
+    ],
   );
 }
 
 Widget storyWidget({
   required double size,
   required String imageUrl,
+  required String text,
+  required bool showGreenStrip,
 }) {
-  return Container(
-    width: size,
-    height: size,
-    decoration: BoxDecoration(
-      shape: BoxShape.circle,
-      image: DecorationImage(
-        image: NetworkImage(imageUrl),
-      ),
+  return Padding(
+    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+    child: Column(
+      children: [
+        Container(
+          width: size,
+          height: size,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            border:
+                showGreenStrip ? Border.all(color: greenColor, width: 2) : null,
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(showGreenStrip ? 2.2 : 0),
+            child: Container(
+              width: size,
+              height: size,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: NetworkImage(imageUrl),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          text,
+          style: TextStyle(color: grayColor.lightShade),
+        )
+      ],
     ),
   );
 }
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -42,21 +42,106 @@ class _HomePageState extends State<HomePage> {
               ],
             ),
             const SizedBox(height: 26),
-            const Divider(thickness: 1),
-            Container(
-              height: 100,
-              child: ListView(
-                shrinkWrap: true,
-                scrollDirection: Axis.horizontal,
-                children: [
-                  addStoryWidget(size: 60, iconData: Icons.add),
-                  storyWidget(
-                      size: 60,
-                      imageUrl:
-                          "https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1173&q=80"),
-                ],
+            const Divider(
+              thickness: 0.7,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.only(left: 16.0, top: 16.0, bottom: 16.0),
+              child: Container(
+                height: 100,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    addStoryWidget(
+                        size: 60, iconData: Icons.add, text: "New Status"),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    Container(
+                      height: 100,
+                      child: ListView(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        children: List.generate(
+                          5,
+                          (index) => storyWidget(
+                            size: 60,
+                            showGreenStrip: true,
+                            text: "Author Name",
+                            imageUrl:
+                                "https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1173&q=80",
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
+            const Divider(
+              thickness: 0.7,
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 16.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          width: 70,
+                          height: 70,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(
+                                  "https://images.unsplash.com/photo-1466112928291-0903b80a9466?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1173&q=80"),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width - 110,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Razique Sir 🫡",
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                    Text(
+                                      "07:51",
+                                      style: TextStyle(
+                                          color: grayColor.lightShade),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 14,
+                              ),
+                              Text(
+                                "Bro, these are fire 🔥🔥",
+                                style: TextStyle(color: grayColor.lightShade),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
